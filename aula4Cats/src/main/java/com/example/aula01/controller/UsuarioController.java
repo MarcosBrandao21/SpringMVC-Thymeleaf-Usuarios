1edcd0a433df17b85692b58671023d049725fbdb
marco <marco@192.168.1.110>
2022-08-02 20:56:23 -0300

    Foi acrescentado a função de deletar o usuario e o botao apagar na tela de usuario.

--- aula4Cats/src/main/java/com/example/aula01/controller/UsuarioController.java
+++ aula4Cats/src/main/java/com/example/aula01/controller/UsuarioController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -42,6 +43,14 @@ public class UsuarioController {
 	public String listarUsuario(Model model) {
 		model.addAttribute("usuarios", usuarioRepository.findAll());
 		return "/auth/admin/admin-listar-usuario";
+	}
+	
+	@GetMapping("/admin/apagar/{id}")
+	public String deleteUser(@PathVariable("id") long id, Model model) {
+		Usuario usuario = usuarioRepository.findById(id)
+				.orElseThrow(() -> new IllegalArgumentException("Id invalido: " + id));
+		usuarioRepository.delete(usuario);
+		return "redirect:/usuario/admin/listar";
 	}
 
 }
