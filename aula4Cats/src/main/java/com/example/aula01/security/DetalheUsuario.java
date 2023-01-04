f9185f7170bc03ebed1503044992e82b0d8278d9
marco <marco@192.168.1.112>
2023-01-04 19:01:17 -0300

    |04/01/2023| Adicionado funcao de aparecer usuario logado e tambem menu com as configuracoes de acesso para cada tipo de permissao

---
+++ aula4Cats/src/main/java/com/example/aula01/security/DetalheUsuario.java
@@ -0,0 +1,80 @@
+package com.example.aula01.security;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import com.example.aula01.modelo.Role;
+import com.example.aula01.modelo.Usuario;
+
+public class DetalheUsuario implements UserDetails {
+
+	private static final long serialVersionUID = -5139378739432852722L;
+	
+	private Usuario usuario;
+
+	public DetalheUsuario(Usuario usuario) {
+		super();
+		this.usuario = usuario;
+	}
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		List<Role> roles = usuario.getRoles();
+		List<SimpleGrantedAuthority> authorities = new ArrayList<>();
+
+		for (Role role : roles) {
+			SimpleGrantedAuthority sga = new SimpleGrantedAuthority(role.getRole());
+			authorities.add(sga);
+		}
+		return authorities;
+	}
+
+	@Override
+	public String getPassword() {
+		// TODO Auto-generated method stub
+		return usuario.getPassword();
+	}
+
+	@Override
+	public String getUsername() {
+		// TODO Auto-generated method stub
+		return usuario.getLogin();
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		// TODO Auto-generated method stub
+		return usuario.isAtivo();
+	}
+
+	public String getNome() {
+		if (!usuario.getNome().isEmpty()) {
+			return usuario.getNome();
+		}
+		return "NOME DE USUARIO NÃO LOCALIZADO";
+	}
+
+}
--- aula4Cats/src/main/java/com/example/aula01/security/DetalheUsuarioServico.java
+++ aula4Cats/src/main/java/com/example/aula01/security/DetalheUsuarioServico.java
@@ -1,20 +1,13 @@
 package com.example.aula01.security;
 
-import java.util.HashSet;
-import java.util.Set;
-
 import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-import com.example.aula01.modelo.Role;
 import com.example.aula01.modelo.Usuario;
 import com.example.aula01.repository.UsuarioRepository;
 
@@ -35,13 +28,8 @@ public class DetalheUsuarioServico implements UserDetailsService {
 		Usuario usuario = usuarioRepository.findByLogin(username);
 
 		if (usuario != null && usuario.isAtivo()) {
-			Set<GrantedAuthority> papeisDoUsuario = new HashSet<GrantedAuthority>();
-			for (Role role : usuario.getRoles()) {
-				GrantedAuthority pp = new SimpleGrantedAuthority(role.getRole());
-				papeisDoUsuario.add(pp);
-			}
-			User user = new User(usuario.getLogin(), usuario.getPassword(), papeisDoUsuario);
-			return user;
+			DetalheUsuario detalheUsuario = new DetalheUsuario(usuario);
+			return detalheUsuario;
 		} else {
 			throw new UsernameNotFoundException("Usuário não encontrado");
 		}
